6978a6ff9a00d5a7aee22e46f7b263e745f395bf
Satshabad Khalsa <satshabad.music@gmail.com>
2012-02-25 13:06:42 -0800

    added boilerplate service for drive listener

--- src/com/cmd/android/radar/MainSettingsActivity.java
+++ src/com/cmd/android/radar/MainSettingsActivity.java
@@ -1,13 +1,25 @@
 package com.cmd.android.radar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainSettingsActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	public static final String LOG_TAG = "Radar-Detector";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Log.d(MainSettingsActivity.LOG_TAG, "MainSettingsActivity created");
+
+		// This starts the DriveListenerService
+		startService(new Intent(MainSettingsActivity.this, DriveListenerService.class));
+
+
+	}
 }
