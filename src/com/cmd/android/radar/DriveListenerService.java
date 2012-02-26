86b20f6ea11351e51c49793e1545885a4d5b707b
Satshabad Khalsa <satshabad.music@gmail.com>
2012-02-25 16:29:50 -0800

    added receiver that is called when wifi is connected OR when wifi goes out of range

--- src/com/cmd/android/radar/DriveListenerService.java
+++ src/com/cmd/android/radar/DriveListenerService.java
@@ -1,13 +1,19 @@
 package com.cmd.android.radar;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 public class DriveListenerService extends Service {
+	
+	private BroadcastReceiver wifiChangedReceiver;
 
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -23,9 +29,28 @@ public class DriveListenerService extends Service {
 	public void onCreate() {
 		Log.d(MainSettingsActivity.LOG_TAG, "DriveListenerService created");
 		super.onCreate();
-
+		final IntentFilter theFilter = new IntentFilter();
+        theFilter.addAction("android.net.wifi.STATE_CHANGE");
+        
+        wifiChangedReceiver = new BroadcastReceiver() {
+			
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				
+			}
+		};
+		this.registerReceiver(this.wifiChangedReceiver, theFilter);
 	}
 	
+	@Override
+    public void onDestroy() {
+        super.onDestroy();
+        
+        this.unregisterReceiver(this.wifiChangedReceiver);
+    }
+
 	
+
+
 
 }
