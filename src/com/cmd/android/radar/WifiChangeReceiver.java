11e99cfe5d45dd7ddfedc8b883d0e919d5bb3156
Satshabad Khalsa <satshabad.music@gmail.com>
2012-02-27 20:31:57 -0800

    replaced long running service with broadcast receiver

---
+++ src/com/cmd/android/radar/WifiChangeReceiver.java
@@ -0,0 +1,22 @@
+package com.cmd.android.radar;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+
+public class WifiChangeReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+			NetworkInfo netInfo = intent
+					.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+			if (netInfo.isConnected()) {
+				// TODO: Launch service to check for driving 
+			}
+		}
+	}
+
+}
