cc9489d920bccc3438d872da117eae4fa3500348
rcmontoy <rcmontoya@csupomona.edu>
2012-02-29 11:03:46 -0800

    Added listener class

---
+++ src/com/cmd/android/radar/ShakeListenerService.java
@@ -0,0 +1,23 @@
+package com.cmd.android.radar;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ShakeListenerService extends Service
+{
+
+	/**
+	 * We dont need
+	 */
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// onstartcommand
+	
+	// on command
+
+}
