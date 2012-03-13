4ac2ab1652ec5ce023f57ad77886088395a023bf
Satshabad Khalsa <satshabad.music@gmail.com>
2012-03-12 23:32:47 -0700

    added launcher for location. Later will be moved to shakelistener

--- src/com/cmd/android/radar/MainSettingsActivity.java
+++ src/com/cmd/android/radar/MainSettingsActivity.java
@@ -1,9 +1,10 @@
 package com.cmd.android.radar;
 
 import android.app.Activity;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.wifi.WifiManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -14,6 +15,8 @@ public class MainSettingsActivity extends Activity {
 	 * not
 	 */
 	public static final String LOG_TAG_CHECK_FOR_DRIVING = "Radar-Detector.checkForDriving";
+	public static final String LOG_TAG_TRAP_REPORT = "Radar-Detector.trapReport";
+	public static final String TIME_REPORTED_PREF_KEY = "TIME_REPORTED_PREF_KEY";
 
 	/** Called when the activity is first created. */
 	@Override
@@ -21,6 +24,15 @@ public class MainSettingsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		Log.d(MainSettingsActivity.LOG_TAG_CHECK_FOR_DRIVING, getPackageName());
-
+		long time = System.currentTimeMillis();
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+		Intent intent = new Intent("com.cmd.android.radar.TRAP_LOCATION_RECEIVED");
+		Bundle bundle = new Bundle();
+		bundle.putLong(TIME_REPORTED_PREF_KEY, time);
+		intent.putExtras(bundle);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),
+		    0, intent, 0);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, pendingIntent);
 	}
 }
